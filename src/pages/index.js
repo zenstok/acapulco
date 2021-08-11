f5661030b57ddf93928690b26f4413a778da322c
Rares Tarabega <raressh_27@yahoo.com>
2021-08-11 03:50:10 +0300

    Under construction

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Acapulco" />
+    <div className="content">
+      <StaticImage
+        style={{borderRadius: '16px', height: '220px'}}
+        src="../images/corina_logo_acapulco.jpeg"
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="Acapulco logo"
+      />
+      <h1 style={{fontFamily: "Fascinate, sans, serif"}}>În construcție</h1>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
